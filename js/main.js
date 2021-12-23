3db33b33ef79b95beaec0e972e34710c1d437842
DarBaala <joker.milan1997gmail.com>
2021-12-23 06:52:37 +0300

    Create: added news section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const mobileSwiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
@@ -12,5 +12,21 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+const reviewsSwiper = new swiperReviews(".reviews-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slide__button--next",
+    prevEl: ".reviews-slide__button--prev",
   },
 });
